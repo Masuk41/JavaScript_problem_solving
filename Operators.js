b6b80dc3bceeacb4224237bf46a102e28d413b56
shariarmasuk017 <shariarmasuk017@gmail.com>
2026-08-25 18:15:48 +0600

    Operators

---
+++ Operators.js
@@ -0,0 +1,93 @@
+
+// 1. ARITHMETIC OPERATOR
+
++    // Addition
+-    // Subtraction
+*    // Multiplication
+/    // Division
+%    // Remainder
+**   // Exponentiation
+++   // Increment
+--   // Decrement
+
+// 2. ASSIGNMENT OPERATOR
+
+=    // Assign
++=   // Add and assign
+-=   // Subtract and assign
+*=   // Multiply and assign
+/=   // Divide and assign
+%=   // Remainder and assign
+**=  // Exponentiation and assign
+
+// 3. COMPARISON OPERATOR
+
+==   // Equal value
+===  // Equal value and type
+!=   // Not equal value
+!==  // Not equal value or type
+>    // Greater than
+<    // Less than
+>=   // Greater than or equal
+<=   // Less than or equal
+
+// 4. LOGICAL OPERATOR
+
+&&   // AND
+||   // OR
+!    // NOT
+
+// 5. TERNARY OPERATO
+
+condition ? value1 : value2;
+
+
+// Example:
+let age = 20;
+let result = age >= 18 ? "Adult" : "Child";
+
+
+
+// 6. TYPE OPERATOR
+
+typeof       // Check data type
+instanceof   // Check object type
+
+console.log(typeof "Hello");  // string
+console.log(typeof 10);      // number
+
+// 9. STRING OPERATOR
+
++    // Join strings
+
+let firstName = "Shariar";
+let lastName = "Masuk";
+
+console.log(firstName + " " + lastName);
+
+// 10. BITWISE OPERATOR
+
+&    // AND
+|    // OR
+<<   // Left shift
+>>   // Right shift
+>>>  // Zero-fill right shift
+
+//EXAMPL
+
+let a = 10;
+let b = 3;
+
+console.log(a + b);   // 13
+console.log(a - b);   // 7
+console.log(a * b);   // 30
+console.log(a / b);   // 3.333
+console.log(a % b);   // 1
+console.log(a ** b);  // 1000
+
+console.log(a > b);   // true
+console.log(a === b); // false
+
+console.log(a > 5 && b < 5); // true
+console.log(a > 20 || b < 5); // true
+console.log(!(a > b)); // false
