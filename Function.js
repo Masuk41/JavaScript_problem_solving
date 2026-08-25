a46056a450e3a8ffef93bf1f05c77199329a20e9
shariarmasuk017 <shariarmasuk017@gmail.com>
2026-08-25 17:11:46 +0600

    void and Template(`)

--- Function.js
+++ Function.js
@@ -4,3 +4,39 @@ function total(price = 10, quantity) {
 }
 
 console.log(total(undefined,3));
+
+// 2.How Do void and Template Literals (``) Work in JavaScript?
+
+function sayName(name){
+   console.log(`Hello ${name}`)
+}
+sayName('Masuk')
+
+// without Template using return
+
+function sayName(name) {
+    return "Hello " + name;
+}
+console.log(sayName("Masuk"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
