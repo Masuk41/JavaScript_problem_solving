785675b93a399c8ac8cf25b2be1e8194045d2a66
shariarmasuk017 <shariarmasuk208@gmail.com>
2026-09-02 08:52:18 +0600

    find(),filter(), includes()

---
+++ 16_Find_incloud_some_Every_Array_Method.js
@@ -0,0 +1,35 @@
+// find(), includes(), some(), and every().
+
+
+// 1.find() — find the first matching item
+let students = [
+  { name: "Sakib", marks: 90 },
+  { name: "Nayeem", marks: 55 },
+  { name: "Hasan", marks: 75 },
+];
+
+let result = students.find((f)=> f.marks>60 )
+
+console.log(result); //{ name: 'Sakib', marks: 90 }
+
+// can not see { name: "Hasan", marks: 75 } this 
+// if get the result then stop 
+
+
+
+// 2.filter()find all the matching item
+
+let fruits =["Apple","Banana","Mango", ]
+
+let healthy_fruits = fruits.filter((fruits)=>    fruits.length<=5)
+ console.log(healthy_fruits);
+
+//  3.includes() — check whether a value exists
+
+ let find1_fruits = fruits.includes("orange")
+ let find2_fruits = fruits.includes("Banana")
+
+ console.log(find1_fruits)//false
+ console.log(find2_fruits)//true
+ 
+
